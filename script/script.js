2653b6934ff8a764a88bff3c6f73d1b6ac582151
Teo <seminara.teodoro93@gmail.com>
2025-09-01 15:59:56 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,29 @@
+// Crea una lista di email di invitati ad una festa.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+// ‼️ Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+// Si può fare? Certo che si basta ragionare un po’.
+// Nota: 
+// Non è necessario provvedere alla validazione delle email
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+// Consigli del giorno:
+// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+// si ma noi cosa vogliamo fare?
+// torniamo a scrivere in italiano
+// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+// Buon lavoro e buon divertimento! 🧙‍♂️
+
+cosnt partyInvite = ["luca.bianchi@gmail.com", "giulia.verdi@gmail.com", "andrea.neri@gmail.com", "sara.romano@gmail.com", "marco.ferrari@gmail.com" ]
+
+console.log(partyInvite);
